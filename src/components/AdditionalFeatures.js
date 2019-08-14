f580d43e85bd8b1dbb284e7c8fc647f718067490
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-14 14:32:06 -0600

    connected AdditionalFeatures.js to reducer.js

--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux'
+
 import AdditionalFeature from './AdditionalFeature';
 
 const AdditionalFeatures = props => {
@@ -18,4 +20,14 @@ const AdditionalFeatures = props => {
   );
 };
 
-export default AdditionalFeatures;
+const mapStateToProps = state => {
+  return {
+    car: state.car,
+    store: state.store
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  {}
+)(AdditionalFeatures);
